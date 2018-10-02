2a03794d2dd6849caf571af91645a0032f8b94be
Riper3 <javier@softpoint.es>
2018-10-02 12:48:24 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const Hapi = require('hapi');
+
+const host = 'localhost';
+const port = 3002;
+
+const server = Hapi.Server({
+    host: host,
+    port: port
+});
+
+const init = async () =>
+{
+
+    await server.start();
+    console.log("Server up and running at port: " + port);
+
+}
+
+require('./services/services')(server);
+
+init();
---
+++ services/services.js
@@ -0,0 +1,59 @@
+module.exports = function(server) {
+
+    server.route({
+        method: 'GET',
+        path: '/about',
+        handler: function (request, h)
+        {
+
+            var data =
+            {
+                description: 'This is a API example using nodejs'
+            };
+            return data;
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/luckynumber/{day}/{month}/{year}',
+        handler: function (request, h)
+        {
+            const date = parseInt(request.params.day)+'/'+parseInt(request.params.month)+'/'+parseInt(request.params.year);
+            const day = parseInt(request.params.day);
+            const month = parseInt(request.params.month);
+            const year = parseInt(request.params.year);
+
+            var datehours = (new Date() / 1000) / 3600;
+            var currentday = Math.trunc(datehours / 24);
+
+            var rawluckynumber = Math.trunc((day + year / month) * currentday);
+            var luckynumber = String(rawluckynumber).slice(-2);
+
+            var rawlotterynumber = String(rawluckynumber).slice(-4);
+            var lotterynumber = rawlotterynumber * 13;
+            if (String(lotterynumber).length != 5)
+            {
+              lotterynumber = "Today you shouldn't play the lottery";
+            }
+
+            var checkdate = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+
+            if(checkdate.test(date))
+            {
+              var result =
+              {
+                  luckytodaynumber : luckynumber,
+                  lotterynumber : lotterynumber
+              };
+
+              return result;
+            }
+            else
+            {
+              return 'Please, check the date';
+            }
+
+        }
+    });
+}
